45594162299256fa9d50ab0f04c85842cbdd107d
PranavMarthi <69437339+PranavMarthi@users.noreply.github.com>
2023-08-19 10:34:46 -0400

    navbar

---
+++ guidebotapp/src/Components/NavBar.js
@@ -0,0 +1,46 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="navbar-dark" style={{
+        backgroundColor:"#3494f4"
+    }}>
+      <Container>
+        <Navbar.Brand href="#home" style={{
+            fontWeight:'bolder',
+            fontSize:'30px'
+        }}>GuideBot</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" style={{
+                paddingLeft: '70px'
+            }}>Home</Nav.Link>
+            <Nav.Link href="#link" style={{
+                paddingLeft: '70px'
+            }}>Purchase</Nav.Link>
+            <Nav.Link href="#link" style={{
+                paddingLeft: '70px'
+            }}>Contact</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
