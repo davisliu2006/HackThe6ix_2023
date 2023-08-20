0d88ee382cdff75c59d0a820958c21d0c7fd2427
Davis Liu <davisliu2006@gmail.com>
2023-08-20 03:29:54 -0400

    W

--- cam-pages/include.js
+++ cam-pages/include.js
@@ -2,6 +2,12 @@
 function round(val, nearest) {
     return Math.floor(val/nearest)*nearest;
 }
+function rad(x) {
+    return x*Math.PI/180;
+}
+function deg(x) {
+    return x*180/Math.PI;
+}
 
 // formating
 function format(str, varArr, spChar = '%') {
@@ -13,4 +19,13 @@ function format(str, varArr, spChar = '%') {
         else {temp += c;}
     }
     return temp;
+}
+
+// data
+function Arr2d(sx, sy) {
+    let arr = new Array(sx);
+    for (let x in arr) {
+        arr[x] = new Array(sy);
+    }
+    return arr;
 }
